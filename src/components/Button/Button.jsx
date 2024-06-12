3809274bb983b3ec60306b579e127bb9408abce7
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-12 15:23:30 +0200

    added button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+// Importo lo style della card
+import buttonStyle from './Button.module.css';
+
+const Button = () => {
+    return (
+        <>
+            <div className={buttonStyle.btn}>
+                Leggi di più
+            </div>
+        </>
+    );
+}
+
+export default Button;
