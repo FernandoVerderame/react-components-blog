85f430942165e9e631dcdbea28ee4ac97175b382
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-12 17:21:23 +0200

    added dinamic posts

--- src/components/Card/PostCard.jsx
+++ src/components/Card/PostCard.jsx
@@ -2,24 +2,26 @@
 import postCardStyle from './PostCard.module.css';
 import Button from '../Button/Button';
 
-const PostCard = () => {
+const PostCard = ({ p }) => {
     return (
         <>
             <div className={postCardStyle.card}>
-                <div className={postCardStyle.image}></div>
+                <div className={postCardStyle.image}>
+                    <img src={p.image} alt={p.title} className={postCardStyle.img} />
+                </div>
                 <div className={postCardStyle.bottom}>
-                    <h3>Napoli. Effetto Conte: boom di trapianti di capelli in città</h3>
+                    <h3>{p.title}</h3>
                     <p className={postCardStyle.paragraph}>
-                        Conte ha dichiarato: "Sono qui per rivoluzionare il calcio napoletano. Scordatevi il gioco offensivo, da oggi si gioca tutti dietro la linea del pallone, compreso il magazziniere".
+                        {p.content}
                     </p>
                     <div className={postCardStyle.flexAuthor}>
                         <Button />
-                        <author className={postCardStyle.author}>Fernando Verderame</author>
+                        <p className={postCardStyle.author}>{p.author}</p>
                     </div>
                 </div>
             </div>
         </>
-    );
+    )
 }
 
 export default PostCard;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,15 @@
 // Importo lo style del main
 import PostCard from '../Card/PostCard';
 import mainStyle from './Main.module.css';
+import posts from '../../data/posts.js';
 
 const Main = () => {
     return (
         <>
             <main className={mainStyle.mainSec}>
-                <PostCard />
+                {posts.map(p => (
+                    <PostCard key={p.id} p={p} />
+                ))}
             </main>
         </>
     );
