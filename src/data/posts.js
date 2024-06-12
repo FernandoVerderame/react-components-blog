b888f764c487c084690e5e0ae422327698f0c806
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-12 17:20:51 +0200

    added posts array

---
+++ src/data/posts.js
@@ -0,0 +1,25 @@
+const posts = [
+    {
+        id: 1,
+        title: "Napoli. Effetto Conte: boom di trapianti di capelli in città",
+        image: "src/assets/images/conte.jpg",
+        content: "Conte ha dichiarato: \"Sono qui per rivoluzionare il calcio napoletano. Scordatevi il gioco offensivo, da oggi si gioca tutti dietro la linea del pallone, compreso il magazziniere\".",
+        author: "Fernando Verderame"
+    },
+    {
+        id: 2,
+        title: "“Raggiunto accordo storico”: Giorgia Meloni ottiene uno sconto dal suo gestore telefonico",
+        image: "src/assets/images/telemeloni.jpg",
+        content: "Dopo lunghe trattative, Giorgia Meloni ha finalmente ottenuto uno sconto sulla sua bolletta telefonica. \"È un giorno storico per l'Italia\", ha dichiarato, \"Finalmente pagherò meno per chiamare mia madre\".",
+        author: "Santiago Galvan Colorado"
+    },
+    {
+        id: 3,
+        title: "Telespettatore afferma di aver visto una pubblicità senza Jannik Sinner",
+        image: "src/assets/images/sinner.jpg",
+        content: "Un evento straordinario ha scosso il mondo della televisione: un telespettatore giura di aver visto una pubblicità senza Jannik Sinner. \"Non ci potevo credere\", ha detto, \"Pensavo fosse uno scherzo\".",
+        author: "Marco Lanci"
+    }
+];
+
+export default posts;
