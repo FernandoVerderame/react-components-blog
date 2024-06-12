1b170b028fa582980335972cd45932182dfe7e77
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-12 15:23:01 +0200

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+// Importo lo style del footer
+import footerStyle from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <>
+            <footer className={footerStyle.footerSec}>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate maxime optio esse. Vero non expedita quisquam esse, at voluptatum reprehenderit commodi odio iure eius cum officiis voluptas mollitia voluptates eveniet.
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
